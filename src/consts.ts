40eb9f8169ad8be9fc0fb03aea71483ad9e2226c
Hugo Luis Lopez Cahuillan <61170631+lpzcahuillan@users.noreply.github.com>
2024-11-19 14:00:39 -0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
+  TITLE: "LPZC",
   DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  AUTHOR: "Hugo Lopez",
 }
 
 // Work Page
